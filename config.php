bf3967d3b7692f7a078b1d03e2e2a662b6120f17
Ashwini Purohit <ashwinipurohit@yahoo.in>
2015-12-03 20:01:20 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,5 @@
 <?php
-<<<<<<< HEAD
-//$mysqli=new mysqli("iitjeeorganic.db.10424157.hostedresource.com","iitjeeorganic","Ashwini@0529","iitjeeorganic");
-=======
->>>>>>> origin/master
+
 $mysqli=new mysqli("merabite.db.10424157.hostedresource.com","merabite","Ashwini@0529","merabite");
 date_default_timezone_set('Asia/kolkata');
 if(mysqli_connect_errno())
