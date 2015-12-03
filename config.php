32a06360b909843ca402506d209322064f4464a9
Ashwini Purohit <ashwinipurohit@yahoo.in>
2015-12-03 19:47:35 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,5 @@
 <?php
-//$mysqli=new mysqli("iitjeeorganic.db.10424157.hostedresource.com","iitjeeorganic","Ashwini@0529","iitjeeorganic");
-$mysqli=new mysqli("mysql.hostinger.in","u860596125_pray","prayforchennainow","u860596125_pray");
+$mysqli=new mysqli("http://mysql.hostinger.in","u860596125_pray","prayforchennainow","u860596125_pray");
 date_default_timezone_set('Asia/kolkata');
 if(mysqli_connect_errno())
 {
