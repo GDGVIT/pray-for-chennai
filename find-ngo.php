0ea33b6220114cb0743c2916b196218153989bb7
Rishi Raj <rishiraj824@gmail.com>
2015-12-04 15:07:26 +0530

    new changes

--- find-ngo.php
+++ find-ngo.php
@@ -60,6 +60,8 @@
                        <center><input class="btn btn-primary"type = "submit"></center>
           </form> 
         </div>
+        </div>
+      
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript" src="js/materialize.min.js"></script>
