39728c5e3a5cc065d007bfda18d7204163c0237c
Rishi Raj <rishiraj824@gmail.com>
2015-12-06 15:35:20 +0530

    railines

---
+++ raillines.php
@@ -0,0 +1,289 @@
+ <!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <meta charset="utf-8">
+    <meta name="language" content="en-us"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="description" content="Team Relief Chennai | We are a small group of people who took up an initiative to help and rescue people stuck up in floods in Chennai.
+    " />
+    <meta name="keywords" content="Chennai, Rains, Floods, pray for chennai, chennai rains, rescue, help, ngo, chennai support, chennai sos, chennai emergency" />
+    <meta name="distribution" content="Global" />
+    <meta name="robots" content="index, follow" />
+    <meta name="revisit-after" content="1 days"/>
+     <meta name="copyright" content="Relief Chennai"/>
+    
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <link rel="stylesheet" type="text/css" href="css/main.css">
+    </head>
+
+    <body>
+<?php
+
+include 'analyticstracking.php';
+
+?><title>ReliefChennai-An initiative by VIT'ains</title>
+
+    
+    <?php
+
+    include 'navbar.php';
+    include 'funcs.php';
+    include 'side-bar1.php';
+    echo '
+
+          <div class="col s12 m12 l9"> <!-- Note that "m8 l9" was added -->
+            <!-- Teal page content
+
+                  This content will be:
+              9-columns-wide on large screens,
+              8-columns-wide on medium screens,
+              12-columns-wide on small screens  -->
+                <br><br>
+       <div class="row">
+       <div class="col s12 l6 m6">
+        <h4> PETROL / DIESEL AVAILABILITY AT INDIANOIL FUEL STATIONS IN CHENNAI</h4>
+<br>
+<p class="bold">The following IndianOil fuel stations in Chennai have petrol and diesel stocks as indicated against each:<br><br>
+
+
+
+1. Chennai Fuels, Karayanchavadi - Both Petrol & Diesel<br>
+
+2. S R Mohan, Poonamalee - Both Petrol & Diesel<br>
+
+3. Sri Ganesha Agencies, Thiruverkadu - Both Petrol & Diesel<br>
+
+4. Preeti Service Station, Sholingur - Both Petrol & Diesel<br>
+
+5. Vel R S Petro, Avadi - Both Petrol & Diesel<br>
+
+6. Sri Sapthagiri Enterprises, Thiruninravur - Both Petrol & Diesel<br>
+
+7. Pathy & Co, Nerkundram - Both Petrol & Diesel<br>
+
+8. Arabian Auto Services, Alapakam - Both Petrol & Diesel<br>
+
+9. Gandhi Service Station, Thirutani - Both Petrol & Diesel<br>
+
+10. GPR agencies - Arakonam - Both Petrol & Diesel<br>
+
+11. Bharathi Agencies – Sholingur - Both Petrol & Diesel<br>
+
+12. Alfa, Chetpet - Both Petrol & Diesel<br>
+
+13. Chennai Auto, Kilpauk – Both Petrol & Diesel<br>
+
+14. Raj, Kilpauk - Only Petrol<br>
+
+15. Thanya, Anna Nagar - Both Petrol & Diesel<br>
+
+16. Mahalakshmi, Purasaiwakkam - Both Petrol & Diesel<br>
+
+17. GKS, Anna Nagar - Both Petrol & Diesel<br>
+
+18. Jaya, Ayavavaram - Both Petrol & Diesel<br>
+
+19. Deepam, Ayanavaram - Both Petrol & Diesel<br>
+
+20. Vijayalakshmi, Veppery - Both Petrol & Diesel<br>
+
+21. Spurtank Road, Chetpet - Both Petrol & Diesel<br>
+
+22. Sri Sai Balaji - Only Diesel<br>
+
+23. Prema, Vyasarpadi- Both Petrol & Diesel<br>
+
+24. Kamala, Perambur - Both Petrol & Diesel<br>
+
+25. Sri Tangaratham Murugan, Perambur - Both Petrol & Diesel<br>
+
+26. Anush, Royapuram - Only Diesel<br>
+
+27. Yuvasri Filling Station, Vaikadu - Both Petrol & Diesel<br>
+
+28. Kathirvel Enterprises, Manali - Both Petrol & Diesel<br>
+
+29. Om Service Station, Ambattur - Both Petrol & Diesel<br>
+
+30. Sri Venkateswara Service Station, Ambatur - Both Petrol & Diesel<br>
+
+31. Poornima Agencies, Moolakadai - Both Petrol & Diesel<br>
+
+32. Sri Raghavendra Filling Station, Kolathur - Both Petrol & Diesel<br>
+
+33. Chandhini Enterprises, Kolathur - Both Petrol & Diesel<br>
+
+34. Thirumal Thunai Agencies, Janapanchatram - Both Petrol & Diesel<br>
+
+35. RMK Filling Station, Kavarapetai - Both Petrol & Diesel<br>
+
+36. Sree Venkatnarayana Enterprises, Nallur
+
+37. Kishore Enterprises, Poochiathipet - Both Petrol & Diesel<br>
+
+38. GVN Agencies, Ponneri - - Both Petrol & Diesel<br>
+
+39. Maury Unit 2, Red Hills - Both Petrol & Diesel<br>
+
+40. Srinivasa Agencies, Red Hills - Both Petrol & Diesel<br>
+
+41. Kathirvel Enterprises, Madhavaram - Both Petrol & Diesel<br>
+
+42. Sri Muthuram Agencies, Arambakkam - Both Petrol & Diesel<br>
+
+43. Sri Balaji Agencies, Gummidipoondi – Only Diesel<br>
+
+44. NRP Agencies – Manali - Both Petrol & Diesel<br>
+
+45. Maurya Agencies – Kolathur - Both Petrol & Diesel<br>
+
+46. BALAJI ENTERPRISES, ULLAGARAM- BOTH PETROL AND DIESEL<br>
+
+47. VSV AGS, KOTTIVAKKAM - BOTH PETROL AND DIESEL<br> 
+48. LALITHA FUEL CENTRE, THIRUMUDIVAKKAM- BOTH PETROL AND DIESEL<br> 
+49. BALAKRISHNA AGS, MMNAGAR- BOTH PETROL AND DIESEL<br> 
+50. COCO PALLAVARAM - BOTH PETROL AND DIESEL<br> 
+51. PACIFIC ENPS, CHROMEPET- BOTH PETROL AND DIESEL<br> 
+52. SGP SHRII WDM AGS, KOLAPAKKAM - BOTH PETROL AND DIESEL<br> 
+53. HANISHA S STN, KOTTIVAKKAM - BOTH PETROL AND DIESEL<br> 
+54. KUMARAN KUNDRAM, HASTINAPURAM- BOTH PETROL AND DIESEL<br> 
+55. PADMAVATHY AGS, INJAMBAKKAM - BOTH PETROL AND DIESEL<br> 
+56. RAGHAVENDRA AGS, KELAMBAKKAM- BOTH PETROL AND DIESEL<br> 
+57. VASANTH ENPS, KOTTIVAKKAM- BOTH PETROL AND DIESELVERIFIED</p>
+</div>
+
+<div class="col l6 s12 m6">
+<h4> LIST OF CANCELLED AND SCHEDULED TRAINS FROM EGMORE on 6th Dec<br><br></h4>
+
+<table class="bordered striped">
+
+        <thead>
+          <tr>
+              <th data-field="id">S.No.</th>
+              <th data-field="name">Scheduled(Train No.)</th>
+              <th data-field="name">Cancelled(Train No.)</th>
+              
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>12164</td>
+            <td>16583</td>
+            
+          </tr></tr>
+
+          <tr>
+            <td>2</td>
+            <td>16127</td>
+            <td>16713</td>
+
+            
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>12651</td>
+            <td>17651</td>
+            
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>11017</td>
+            <td>17643</td>
+            
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>12635</td>
+            <td>12633</td>
+            
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>12605</td>
+            <td>12642</td>
+            
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>16105</td>
+            <td>12631</td>
+            
+          </tr>
+          <tr>
+            <td>8</td>
+            <td>16115</td>
+            <td>18495</td>
+            
+          </tr>
+          <tr>
+            <td>9</td>
+            <td>12693</td>          
+            <td>22623</td>
+            
+          </tr>
+          <tr>
+            <td>10</td>
+            <td>16723</td>
+            <td>11063</td>
+            
+          </tr>
+          <tr>
+            <td>11</td>
+            <td>12661</td>
+            <td>16175</td>
+            
+          </tr>
+          <tr>
+            <td>12</td>
+            <td>12637</td>
+            
+          </tr>
+          <tr>
+            <td>13</td>
+            <td>16101</td>
+            
+          </tr>
+          <tr>
+            <td>14</td>
+            <td>16179</td>
+            
+          </tr>
+          <tr>
+            <td>15</td>
+            <td>16859</td>
+            
+          </tr>
+          <tr>
+            <td>16</td>
+            <td>16177</td>
+            
+          </tr>
+          <tr>
+            <td>17</td>
+           <td>16183</td>
+            
+          </tr>
+
+        </tbody>
+      </table>
+
+
+</div>
+</div>';
+
+    ?>
+   
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+  </html> 
